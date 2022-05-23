64a22ec80b34b3cae1f519f42e1252025c02d9f0
LeoPradop <leonardoguprado@gmail.com>
2022-05-23 02:48:50 -0500

    Deploy changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
-  production: true
-};
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDYc7z7OaEa_bWED2sDiTacuT2sEjtBjAI",
-  authDomain: "surebetsy-3c1e6.firebaseapp.com",
-  projectId: "surebetsy-3c1e6",
-  storageBucket: "surebetsy-3c1e6.appspot.com",
-  messagingSenderId: "266704611084",
-  appId: "1:266704611084:web:701c33e3f6f6601c2e0b29"
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDYc7z7OaEa_bWED2sDiTacuT2sEjtBjAI",
+    authDomain: "surebetsy-3c1e6.firebaseapp.com",
+    projectId: "surebetsy-3c1e6",
+    storageBucket: "surebetsy-3c1e6.appspot.com",
+    messagingSenderId: "266704611084",
+    appId: "1:266704611084:web:701c33e3f6f6601c2e0b29"
+  }
 };
